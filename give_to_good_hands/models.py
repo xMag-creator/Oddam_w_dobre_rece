c6af72787fdc3252ffa20ffe54e334140c6548df
Przemysław Kobylarz <p.kobylarz@outlook.com>
2021-10-19 21:32:02 +0200

    app, models, templates, static files added

---
+++ give_to_good_hands/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+TYPE_CHOICE = (
+    (0, 'Fundacja'),
+    (1, 'Organizacja pozarządowa'),
+    (2, 'Zbiórka lokalna'),
+)
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICE, default=0)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=64)
+    city = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
