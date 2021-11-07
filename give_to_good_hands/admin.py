b74b70c332d6a6c22700ac75a364518dccbd9c45
Przemysław Kobylarz <p.kobylarz@outlook.com>
2021-11-07 21:42:07 +0100

    admin flields updated

--- give_to_good_hands/admin.py
+++ give_to_good_hands/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Institution
+from django.contrib.auth.admin import UserAdmin
+from give_to_good_hands.models import Institution
 
 admin.site.unregister(User)
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
+    user_fields = ['first_name', 'last_name', 'email', 'is_active']
+    superuser_fields = ['is_staff', ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        if request.user.is_superuser:
+            self.fields = self.user_fields + self.superuser_fields
+        else:
+            self.fields = self.user_fields
+
+        return super(CustomUserAdmin, self).get_form(request, obj, **kwargs)
+
     def delete_queryset(self, request, queryset):
         user = request.user
         if user not in queryset:
@@ -14,7 +26,7 @@ class UserAdmin(admin.ModelAdmin):
             admins_to_delete = queryset.filter(is_staff=True, is_superuser=False)
             print(len(admins_to_delete))
             if (len(admins) - len(admins_to_delete)) > 1:
-                super(UserAdmin, self).delete_queryset(request, queryset)
+                super(CustomUserAdmin, self).delete_queryset(request, queryset)
             else:
                 print("it must be minimum 1 admin")
         else:
@@ -23,10 +35,8 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Institution)
 class InstitutionAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'type', 'categories_list']
+    list_display = ('name', 'description', 'type', 'categories_list')
+
 
     def categories_list(self, obj):
         return ", ".join([str(category) for category in obj.categories.all()])
-
-
-# Register your models here.
