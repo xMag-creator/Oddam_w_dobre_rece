671b031e5dca08f1f769d0de4acd5f4f92711b71
Przemysław Kobylarz <p.kobylarz@outlook.com>
2021-11-07 19:09:10 +0100

    validation added

--- give_to_good_hands/admin.py
+++ give_to_good_hands/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from django.db.models import Model
-from give_to_good_hands.models import Institution
+from django.contrib.auth.models import User
+from .models import Institution
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        user = request.user
+        if user not in queryset:
+            admins = User.objects.filter(is_staff=True, is_superuser=False)
+            admins_to_delete = queryset.filter(is_staff=True, is_superuser=False)
+            print(len(admins_to_delete))
+            if (len(admins) - len(admins_to_delete)) > 1:
+                super(UserAdmin, self).delete_queryset(request, queryset)
+            else:
+                print("it must be minimum 1 admin")
+        else:
+            print("user is in queryset")
+
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'type', 'categories_list']
+
+    def categories_list(self, obj):
+        return ", ".join([str(category) for category in obj.categories.all()])
+
 
 # Register your models here.
-admin.site.register(Institution)
-
